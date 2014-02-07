3135ce3f75c4a437c5a621a2653de1d8cd27084a
Lachlan Crawford <lachlan@Titanic>
2014-02-07 22:13:46 +0000

    File gathering procedure updated to link artists, albums and tracks.

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -11,6 +11,16 @@ class FilesController# < ApplicationController
 			@tracks = Track.all
 		end
 		
+		if (@albums.nil?) then
+			@albums = Album.all
+		end
+		
+		if (@artists.nil?) then
+			@artists = Artist.all
+		end
+		
+		albums_to_update = []
+		
 		root = location.location
 		
 		found_files = getFiles(root)
@@ -27,26 +37,32 @@ class FilesController# < ApplicationController
 		}
 		
 		new_tracks = []
-		files.each{|file|
+		files.each do |file|
 			TagLib::FileRef.open(file) do |fileref|
 				unless fileref.null?
-					#artist = fileref.tag.artist
-					#album = fileref.tag.album
+					artistName = fileref.tag.artist
+					albumTitle = fileref.tag.album
 					title = fileref.tag.title
-					#trackNo = fileref.tag.track
-					new_tracks.push({:title => title, :location => file, :album_id => nil, :root_id => location})
-
-					#music.addTrack(title,trackNo,files[i],album,artist)
-					#track = Track.new(title: title, location: file, album_id: nil)
-					#if (!track.save) then
-					#	@fails.push(file)
-					#end
+					trackNo = fileref.tag.track
+					
+					artist = get_artist artistName
+					
+					album, existingAlbum = get_album albumTitle, artist
+					
+					if (existingAlbum && !albums_to_update.include?(album)) then
+						albums_to_update.push(album)
+					end
+					
+					new_tracks.push({:title => title, :location => file, :album => album, :root_id => location, :track_no => trackNo, :artist => artist})
 				end
 			end
-		}
+		end
+		
 		Track.transaction do
 			Track.create(new_tracks)
 		end
+		
+		update_album_artist albums_to_update
 	end
 	
 	def discard(location)
@@ -57,6 +73,94 @@ class FilesController# < ApplicationController
 	
 	private
 
+	
+	def get_artist(artistName)
+		artist = nil
+		
+		if (!artistName.nil? && artistName.strip != "") then
+			artistName.strip!
+			artistNameLower = artistName.downcase
+			artistIndex = @artists.find_index{|artist|artist.name.downcase == artistNameLower}
+			
+			if (artistIndex.nil?) then
+				artist = Artist.new({:name => artistName })
+				if (!artist.save) then
+					artist = nil
+				else
+					@artists.push(artist)
+				end
+			else
+				artist = @artists[artistIndex]
+			end
+		end
+		
+		artist
+	end
+	
+	def get_album(albumTitle, artist)
+		album = nil
+		existing = false
+		
+		if (!albumTitle.nil? && albumTitle.strip != "") then
+			albumTitle.strip!
+			albumTitleLower = albumTitle.downcase
+			albumIndex = @albums.find_index{|album|album.title.downcase == albumTitleLower}
+			
+			if (albumIndex.nil?) then
+				album = Album.new({:title => albumTitle, :artist => artist})
+				if (!album.save) then
+					album = nil
+				else
+					@albums.push(album)
+				end
+			else
+				album = @albums[albumIndex]
+				existing = true
+			end
+		end
+		
+		[album, existing]
+	end
+	
+	def update_album_artist(albums_to_update)
+		various_artist = get_artist "Various"
+		unknown_artist = get_artist "Unknown"
+		update_albums = []
+		
+		albums_to_update.each do |album|
+			albumArtist = album.artist
+			if (albumArtist.nil?) then
+				albumArtist = unknown_artist
+				album.artist = unknown_artist
+			end
+			
+			tracks = album.tracks
+			
+			artists = tracks.map{|track|track.artist}
+			
+			matching = true
+			prev = nil
+			
+			artists.each do |artist|
+				if (!artist.nil?) then
+					if (!prev.nil? && artist.name != prev) then
+						matching = false
+					end
+					
+					prev = artist.name
+				end
+			end
+			
+			if (!matching && albumArtist.name != "Various") then
+				album.artist = various_artist
+				update_albums.push album
+			end
+		end
+		
+		Album.transaction do
+			update_albums.each{|album| album.save}
+		end
+	end
 	@@unrecognisedFileExtensions = {}
 	@@fileExtensions = [".mp3",".wma",".wav"]
 
