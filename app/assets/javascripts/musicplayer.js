269784d41d60f824f706187b1fa039e33a67af33
nessiedude <lachlancrawford@hotmail.co.uk>
2014-03-22 15:21:28 +0000

    Enabled playing all tracks from an artist or album.

---
+++ app/assets/javascripts/musicplayer.js
@@ -0,0 +1,107 @@
+function MusicPlayer(){
+	
+	var isInitialised = false;
+	var isPlaying = false;
+	var playlist = [];
+	var currentTrackIndex = -1
+	var audio = null;
+	var idString = "musicplayer";
+	var timeCompleteDiv = null;
+	var timeRemainingDiv = null;
+	var progressDiv = null;
+	
+	this.initialise = function(){
+		if (isInitialised){return;}
+		
+		var container = document.getElementById(idString);
+		if (!container){return;}
+		
+		container.innerHTML = "<div id='musicplayer_progress'><div id='musicplayer_complete'></div><div id='musicplayer_remaining'></div></div><div class='controls clearfix'><div class='button' id='musicplayer_sb'>SkipBackward</div><!--<div class='button' id='musicplayer_rw'>Rewind</div>--><div class='button' id='musicplayer_pp'>Play</div><!--<div class='button' id='musicplayer_ff'>FastForward</div>--><div class='button' id='musicplayer_sf'>SkipForward</div></div>";
+	
+		audio = new Audio();
+		audio.controls = false;
+		container.appendChild(audio);
+		
+		timeCompleteDiv = document.getElementById(idString + "_complete");
+		timeRemainingDiv = document.getElementById(idString + "_remaining");
+		progressDiv = document.getElementById(idString + "_progress");
+		
+		var thisObject = this;
+		document.getElementById(idString + "_pp").onclick = function(){togglePlaying(this);};
+		document.getElementById(idString + "_sf").onclick = skipForward;
+		document.getElementById(idString + "_sb").onclick = skipBackward;
+		audio.onended = audio.onerror = audio.onstalled = skipForward;
+		audio.ontimeupdate = updateProgress;
+		
+		isInitialised = true;
+	}
+	
+	function updateProgress(){
+		var completeWidth = progressDiv.offsetWidth;
+		var currentTime = Math.round((audio.currentTime * completeWidth) / audio.duration);
+		var remainingTime = completeWidth - currentTime;
+		timeCompleteDiv.setAttribute("style","width:" + currentTime + "px");
+		timeRemainingDiv.setAttribute("style","width:" + remainingTime + "px");
+	}
+	
+	function togglePlaying(button){
+		if (isPlaying)
+		{
+			audio.pause();
+			isPlaying = false;
+			button.innerHTML = "Play";
+		}
+		else
+		{
+			audio.play();
+			isPlaying = true;
+			button.innerHTML = "Pause";
+		}
+	}
+	this.togglePlaying = togglePlaying;
+	
+	function skipForward(){
+		if (currentTrackIndex >= 0 && currentTrackIndex < playlist.length - 1){
+			currentTrackIndex++;
+			moveToCurrentIndex();
+		}
+	}
+	this.skipForward = skipForward;
+	
+	function skipBackward(){
+		if (currentTrackIndex > 0){
+			currentTrackIndex--;
+			moveToCurrentIndex();
+		}
+	}
+	this.skipBackward = skipBackward;
+	
+	function moveToCurrentIndex(){
+		audio.src = playlist[currentTrackIndex].src;
+		audio.load;
+		if (isPlaying){
+			audio.play();
+		}
+	}
+	
+	this.addToPlaylist = function(track){
+		var load = playlist.length == 0;
+		
+		if (track instanceof Array){
+			for (var i = 0; i < track.length; i++){
+				playlist.push(track[i]);
+			}
+		}
+		else{
+			playlist.push(track);
+		}
+		
+		if (load){
+			audio.src = track.src;
+			audio.load();
+			currentTrackIndex = 0;
+		}
+	};
+	
+	this.initialise();
+}
