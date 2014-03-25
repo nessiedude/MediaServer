1709692890ab6c65d408dcfc8a7f36cd1b16f546
nessiedude <lachlancrawford@hotmail.co.uk>
2014-03-25 20:20:10 +0000

    Corrected asset retrieval for musicplayer button images.

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,18 @@
 module TracksHelper
+	def self.musicplayer_script(tracks)
+		html = []
+		html.push('<script>')
+		html.push('var player = new MusicPlayer();')
+		tracks.each {|track|
+			html.push('player.addToPlaylist({title: "')
+			html.push(track.title.to_s)
+			html.push('", src: "')
+			html.push(Rails.application.routes.url_helpers.stream_url(track, :only_path => true))
+			html.push('", duration: ')
+			html.push(track.duration.to_s)
+			html.push('});')
+		}
+		html.push('</script>')
+		html.join.html_safe
+	end
 end
